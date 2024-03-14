d2e54f613e1d65b282ccd373fa82f58ffdcca850
Blottiere Paul <blottiere.paul@gmail.com>
2024-03-14 13:57:20 +0100

    Fixes qsa-cli

--- qsa-cli/qsa/cli.py
+++ qsa-cli/qsa/cli.py
@@ -1,15 +1,42 @@
 # coding: utf8
 
+import os
+import json
 import click
 import requests
 from tabulate import tabulate
 
+QSA_URL=os.environ.get("QSA_SERVER_URL", "http://localhost:5000/")
 
-@click.command()
-@click.argument("url")
-def run(url):
-    url = f"{url}/api/instances"
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def instances():
+    url = f"{QSA_URL}/api/instances"
     data = requests.get(url)
-    j = data.json()
 
-    print(tabulate(j, headers="keys"))
+    headers = ["INSTANCE ID", "IP", "STATUS"]
+    table = []
+
+    for s in data.json()["servers"]:
+        t = []
+        t.append(s["id"])
+        t.append(s["ip"])
+        t.append(f"Binded {s['binded']} seconds ago")
+
+        table.append(t)
+
+    print(tabulate(table, headers=headers))
+
+
+@cli.command()
+@click.argument("id")
+def instance(id):
+    url = f"{QSA_URL}/api/instances/{id}"
+    data = requests.get(url)
+
+    print(json.dumps(data.json(), indent=2))
