b37186b3d3f7c062a311f516cac4aae7b4effa00
Blottiere Paul <blottiere.paul@gmail.com>
2024-04-24 15:50:50 +0200

    Add more tests for PostgreSQL storage

--- qsa-api/tests/test_api_storage_postgresql.py
+++ qsa-api/tests/test_api_storage_postgresql.py
@@ -77,3 +77,55 @@ class APITestCasePostgresql(unittest.TestCase):
 
         # remove last project
         p = self.app.delete(f"/api/projects/{TEST_PROJECT_1}")
+
+    def test_layers(self):
+        # add project
+        data = {}
+        data["name"] = TEST_PROJECT_0
+        data["author"] = "pblottiere"
+        p = self.app.post("/api/projects/", data)
+        self.assertEqual(p.status_code, 201)
+
+        # 0 layer
+        p = self.app.get(f"/api/projects/{TEST_PROJECT_0}/layers")
+        self.assertEqual(p.get_json(), [])
+
+        # add layer
+        data = {}
+        data["name"] = "layer0"
+        data["datasource"] = f"{GPKG}|layername=polygons"
+        data["crs"] = 4326
+        data["type"] = "vector"
+        p = self.app.post(f"/api/projects/{TEST_PROJECT_0}/layers", data)
+        self.assertEqual(p.status_code, 201)
+
+        data = {}
+        data["name"] = "layer1"
+        data["datasource"] = f"{GPKG}|layername=lines"
+        data["crs"] = 4326
+        data["type"] = "vector"
+        p = self.app.post(f"/api/projects/{TEST_PROJECT_0}/layers", data)
+        self.assertEqual(p.status_code, 201)
+
+        data = {}
+        data["name"] = "layer2"
+        data["datasource"] = f"{GPKG}|layername=points"
+        data["crs"] = 4326
+        data["type"] = "vector"
+        p = self.app.post(f"/api/projects/{TEST_PROJECT_0}/layers", data)
+        self.assertEqual(p.status_code, 201)
+
+        data = {}
+        data["name"] = "layer3"
+        data["datasource"] = f"{GEOTIFF}"
+        data["crs"] = 4326
+        data["type"] = "raster"
+        p = self.app.post(f"/api/projects/{TEST_PROJECT_0}/layers", data)
+        self.assertEqual(p.status_code, 201)
+
+        # 3 layers
+        p = self.app.get(f"/api/projects/{TEST_PROJECT_0}/layers")
+        self.assertEqual(p.get_json(), ["layer0", "layer1", "layer2", "layer3"])
+
+        # remove last project
+        p = self.app.delete(f"/api/projects/{TEST_PROJECT_0}")
