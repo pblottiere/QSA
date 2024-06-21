5462765d803865a97e842440639cff55f75e035a
Blottiere Paul <blottiere.paul@gmail.com>
2024-06-21 17:52:57 +0200

    Update QSA app to be used with a proper WSGI HTTP production server

---
+++ qsa-api/qsa_api/app.py
@@ -0,0 +1,42 @@
+# coding: utf8
+
+import click
+from flask import Flask
+
+from qsa_api.config import QSAConfig
+from qsa_api.monitor import QSAMonitor
+from qsa_api.api.projects import projects
+from qsa_api.api.symbology import symbology
+from qsa_api.api.instances import instances
+
+app = Flask(__name__)
+
+
+class QSA:
+    def __init__(self) -> None:
+        self.cfg = QSAConfig()
+        if not self.cfg.is_valid:
+            return
+
+        self.monitor = None
+        if self.cfg.monitoring_port:
+            self.monitor = QSAMonitor(self.cfg)
+            self.monitor.start()
+
+        app.config["CONFIG"] = self.cfg
+        app.config["MONITOR"] = self.monitor
+        app.register_blueprint(projects, url_prefix="/api/projects")
+        app.register_blueprint(symbology, url_prefix="/api/symbology")
+        app.register_blueprint(instances, url_prefix="/api/instances")
+
+        app.logger.setLevel(self.cfg.loglevel)
+
+    def run(self):
+        app.run(host="0.0.0.0", threaded=False)
+
+qsa = QSA()
+
+
+@click.command()
+def run():
+    qsa.run()
