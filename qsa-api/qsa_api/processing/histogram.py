56ee29610c3de317003692ca6af7649e5ff2ee1a
Blottiere Paul <blottiere.paul@gmail.com>
2024-07-16 16:43:32 +0200

    Add histogram process

---
+++ qsa-api/qsa_api/processing/histogram.py
@@ -0,0 +1,41 @@
+# coding: utf8
+
+
+class Histogram:
+    def __init__(self, project_uri: str, layer: str) -> None:
+        self.layer = layer
+        self.project_uri = project_uri
+
+    def process(self) -> (bool, dict):
+        # Some kind of cache is bothering us because when a raster layer is
+        # added on S3, we cannot open it with GDAL provider later. The
+        # QgsApplication needs to be restarted... why???
+        manager = Manager()
+        out = manager.dict()
+
+        p = Process(
+            target=Histogram._process,
+            args=(self.project_uri, self.layer, out),
+        )
+        p.start()
+        p.join()
+
+        if "histo" in out:
+            return out["histo"]
+
+        return {}
+
+    @staticmethod
+    def _process(
+        project_uri: str, layer: str, out: dict
+    ) -> None:
+
+        project = QgsProject.instance()
+        project.read(project_uri)
+        lyr = project.mapLayersByName(layer)[0]
+
+        histo = {}
+        for band in range(lyr.bandCount()):
+           histo[band+1] = lyr.dataProvider().histogram(band+1).histogramVector
+
+        out["histo"] = histo
