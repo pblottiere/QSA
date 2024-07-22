d43e98b4add89994c5fd08bb881da3731acebfa1
Blottiere Paul <blottiere.paul@gmail.com>
2024-07-22 15:59:35 +0200

    Add min/max/count parameters for histograms

--- qsa-api/qsa_api/api/processing.py
+++ qsa-api/qsa_api/api/processing.py
@@ -50,8 +50,35 @@ def raster_calculator(project: str):
     return jsonify(rc), 201
 
 
-@processing.get("/raster/histogram/<project>/<layer>")
+@processing.post("/raster/histogram/<project>/<layer>")
 def raster_histogram(project: str, layer: str):
+    schema = {
+        "type": "object",
+        "properties": {
+            "min": {"type": "number"},
+            "max": {"type": "number"},
+            "count": {"type": "number"},
+        },
+    }
+
+    data = request.get_json()
+    try:
+        validate(data, schema)
+    except ValidationError as e:
+        return {"error": e.message}, 415
+
+    mini = None
+    if "min" in data:
+        mini = data["min"]
+
+    maxi = None
+    if "max" in data:
+        maxi = data["max"]
+
+    count = 1000
+    if "count" in data:
+        count = data["count"]
+
     proj = QSAProject(project)
     if proj.exists():
         layer_infos = proj.layer(layer)
@@ -61,7 +88,7 @@ def raster_histogram(project: str, layer: str):
                     "error": "Histogram is available for raster layer only"
                 }
             histo = Histogram(proj._qgis_project_uri, layer)
-            return jsonify(histo.process()), 201
+            return jsonify(histo.process(mini, maxi, count)), 201
         else:
             return {"error": "Layer does not exist"}, 415
     else:
